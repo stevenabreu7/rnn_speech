9ceef6e97e9710442c6bbb80729632191335a730
Steven Abreu <stevenabreu7@gmail.com>
2019-04-24 17:35:46 +0200

    well

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import model
 # TODO correct output size (check the maps in `tools`)
 # TODO correct hidden size (trial and error? research?)
 rnn = model.RecurrentModel(13, 500, 5, 30)
-print(rnn.parameters())
 
 # initializer data loaders
 val_loader = data.val_loader()
@@ -14,7 +13,7 @@ train_loader = data.train_loader()
 # initialize trainer
 # TODO correct number of epochs for training
 n_epochs = 10
-trainer = model.ModelTrainer(model, val_loader, train_loader, n_epochs)
+trainer = model.ModelTrainer(rnn, val_loader, train_loader, n_epochs)
 
 # run the training
 trainer.train()
